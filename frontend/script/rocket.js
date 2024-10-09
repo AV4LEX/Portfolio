123af1a5a156c4eef4f49fcbb4d94535b546b9d2
alex.vaslin <vaslinalex@gmail.com>
2024-10-09 16:20:42 +0200

    trying to fix the focus blue square during 1st anim on mobile

--- frontend/script/rocket.js
+++ frontend/script/rocket.js
@@ -6,9 +6,13 @@ const portal = document.getElementById('portal');
  * Déplace la fusée en diagonale, fait apparaître le portail, puis gère la réapparition de la fusée.
  */
 rocket.addEventListener('click', () => {
+    // Retire le focus après un léger délai
     setTimeout(() => {
-        rocket.blur(); // Retire le focus immédiatement après le clic
-    }, 50); // Délai 
+        rocket.blur(); // Retire le focus 
+    }, 4); // Délai
+
+    // Désactiver temporairement les événements de souris
+    rocket.style.pointerEvents = 'none';
 
     // Animation pour faire disparaître la fusée
     rocket.style.transition = 'transform 0.7s ease-out, opacity 0.7s ease-out';
@@ -23,7 +27,7 @@ rocket.addEventListener('click', () => {
     // Réapparition de la fusée
     setTimeout(() => {
         rocket.style.transition = 'none';
-        rocket.style.transform = 'translate(0, -480px)';
+        rocket.style.transform = 'translate(0, -480px)'; 
         rocket.style.opacity = '0';
 
         setTimeout(() => {
@@ -31,14 +35,17 @@ rocket.addEventListener('click', () => {
             rocket.style.transition = 'transform 1s ease-out, opacity 1s ease-out';
             rocket.style.opacity = '1';
 
-            // Ajouter une rotation après être revenu à la position d'origine
+            // Réactiver les événements de souris
             setTimeout(() => {
+                rocket.style.pointerEvents = 'auto'; 
+
+                // Ajouter une rotation 
                 rocket.style.transition = 'transform 2s ease-out'; 
                 rocket.style.transform = 'rotate(-45deg)'; 
 
                 setTimeout(() => {
                     rocket.style.transition = 'transform 2s ease-out, opacity 0.5s ease-out';
-                    rocket.style.transform = 'translate(0, 0) rotate(0deg)'; 
+                    rocket.style.transform = 'translate(0, 0) rotate(0deg)'; // Remettre la rotation à 0 
 
                     setTimeout(() => {
                         rocket.style.zIndex = '2000'; 
@@ -49,5 +56,5 @@ rocket.addEventListener('click', () => {
             }, 0); 
             
         }, 250); // Réapparition de la fusée 
-    }, 1300); // Délai avant la réapparition
+    }, 1300); // Délai avant réapparition
 });
